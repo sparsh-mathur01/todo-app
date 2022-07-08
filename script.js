8e8fb69f13da9f7f9dd8a2ba942bcf464361cc24
sparsh-mathur01 <108528050+sparsh-mathur01@users.noreply.github.com>
2022-07-08 14:40:39 +0530

    create,delete,read functionality added

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //state
-let todos = [{ label: "test", completed: false }];
+// let todos = [{ label: "test", completed: false }];
+let todos = JSON.parse(localStorage
+    .getItem('todos')) || []
 
 //selectors
 
@@ -7,12 +9,17 @@ let form = document.forms.todos;
 let input = form.elements.todo;
 let list = document.querySelector("ul");
 let count = document.querySelector(".items-count");
-
-
+let last = document.querySelector(".last");
+let show = "all"
 
 
 
 //handlers
+
+function saveToStorage(todos) {
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
 function addtodo(e) {
     e.preventDefault();
     let todo = e.target.todo;
@@ -28,22 +35,84 @@ function addtodo(e) {
     todo.focus;
 }
 
-function rendertodo(todos) {
-    content = ""
-    todos.forEach((todo,index) => {
-        content += `
-    <li data-id=${index}>
-    <input type="checkbox" name="completed">
-    <span>${todo.label}</span>
-    </li>
-    `;
-    });
-    list.innerHTML = content;
-    count.innerHTML = todos.length;
+function rendertodo(todos, filter = 'all') {
+
+    switch (filter) {
+        case "all": {
+            console.log("all todos must be shown");
+            content = ""
+            todos.forEach((todo, index) => {
+                content += `
+            <li data-id=${index}>
+            <input type="checkbox" name="completed" ${todo['completed'] ? "checked" : ""}>
+            <span>${todo.label}</span>
+            <button class="delete-todo"></button>
+            </li>
+            `;
+            });
+            list.innerHTML = content;
+            count.innerHTML = todos.filter(todo => !todo['completed']).length;
+            break;
+        }
+        case "active": {
+            console.log("active todos must be shown");
+            content = ""
+            todos.forEach((todo, index) => {
+                if (!todo['completed']) {
+
+                    content += `
+                    <li data-id=${index}>
+                    <input type="checkbox" name="completed" ${todo['completed'] ? "checked" : ""}>
+                    <span>${todo.label}</span>
+                    </li>
+                    `;
+                }
+            });
+            list.innerHTML = content;
+            count.innerHTML = todos.filter(todo => !todo['completed']).length;
+            break;
+        }
+        case "completed": {
+            console.log("completed todos must be shown");
+            content = ""
+            todos.forEach((todo, index) => {
+                if (todo['completed']) {
+                    content += `
+                    <li data-id=${index}>
+                    <input type="checkbox" name="completed" ${todo['completed'] ? "checked" : ""}>
+                    <span>${todo.label}</span>
+                    </li>
+                    `;
+                }
+            });
+            list.innerHTML = content;
+            count.innerHTML = todos.filter(todo => !todo['completed']).length;
+            break;
+        }
+    }
+    saveToStorage(todos);
 }
 
 function handleclick(e) {
-    console.log(e.target.parentNode)
+    if (e.target.type === "checkbox") {
+        let complete = e.target.checked;
+        let index = e.target.parentNode.getAttribute('data-id');
+        todos[index]["completed"] = complete;
+        rendertodo(todos, show);
+    }
+    else if (e.target.classList[0] === "filter") {
+        show = e.target.classList[1]
+        rendertodo(todos, show)
+    }
+    else if (e.target.classList[0]==="todos-clear" && confirm("remove the completed todos?")){
+        todos=todos.filter(todo=>!todo['completed']);
+        rendertodo(todos);
+    }
+    else if(e.target.classList[0]==='delete-todo'){
+        let index=e.target.parentNode.getAttribute('data-id');
+        todos=[...todos.slice(0,index),...todos.slice(index+1)]
+        rendertodo(todos,show)
+    }
 }
 
 
@@ -54,6 +123,7 @@ function init() {
     rendertodo(todos);
     form.addEventListener("submit", addtodo);
     list.addEventListener("click", handleclick);
+    last.addEventListener("click", handleclick)
 
 }
 init();
