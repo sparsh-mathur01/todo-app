ab2c6073d3b401a04b2829e847399e1780450026
sparsh-mathur01 <108528050+sparsh-mathur01@users.noreply.github.com>
2022-07-20 13:01:16 +0530

    final commit

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ let input = form.elements.todo;
 let list = document.querySelector("ul");
 let count = document.querySelector(".items-count");
 let last = document.querySelector(".last");
+let mode = document.querySelector(".top div")
 let show = "all"
 
 
@@ -48,9 +49,9 @@ function rendertodo(todos, filter = 'all') {
             <button class="delete-todo"><i class='fa fa-trash'></i></button>
             </li>
             `;
-        });
-        list.innerHTML = content;
-        count.innerHTML = todos.filter(todo => !todo['completed']).length;
+            });
+            list.innerHTML = content;
+            count.innerHTML = todos.filter(todo => !todo['completed']).length;
             break;
         }
         case "active": {
@@ -103,26 +104,58 @@ function handleclick(e) {
         show = e.target.classList[1]
         rendertodo(todos, show)
     }
-    else if (e.target.classList[0]==="todos-clear" && confirm("remove the completed todos?")){
-        todos=todos.filter(todo=>!todo['completed']);
+    else if (e.target.classList[0] === "todos-clear" && confirm("remove the completed todos?")) {
+        todos = todos.filter(todo => !todo['completed']);
         rendertodo(todos);
     }
-    else if(e.target.classList[0]==='fa'){
-        let index=parseInt(e.target.parentNode.parentNode.getAttribute('data-id'));
-        todos=[...todos.slice(0,index),...todos.slice(index+1)]
-        rendertodo(todos,show)
+    else if (e.target.classList[0] === 'fa') {
+        let index = parseInt(e.target.parentNode.parentNode.getAttribute('data-id'));
+        todos = [...todos.slice(0, index), ...todos.slice(index + 1)]
+        rendertodo(todos, show)
     }
 }
 
+function handlemode(e) {
+    let ch = e.target.getAttribute('mode')
+    if (ch === 'light') {
+        e.target.src = "./images/icon-sun.svg"
+        document.body.style.background = "url('./images/bg-desktop-dark.jpg')  hsl(235, 21%, 11%) no-repeat";
+        document.querySelector('.box').style.backgroundColor="hsl(235, 24%, 19%)";
+        document.querySelector('.form form input').style.background="hsl(235, 24%, 19%)";
+        Array.from(document.getElementsByTagName('li')).forEach((item)=>{
+            item.style.color="grey";
+        })
+        Array.from(document.getElementsByTagName('p')).forEach((item)=>{
+            item.style.color="grey";
+        })
+        Array.from(document.getElementsByTagName('section')).forEach((item)=>{
+            item.style.color="grey";
+        })
+        e.target.setAttribute('mode', 'dark');
+    }
+    else {
+        e.target.src = "./images/icon-moon.svg"
+        document.body.style.background = "url('./images/bg-desktop-light.jpg')  white no-repeat";
+        document.querySelector('.box').style.backgroundColor="white";
+        document.querySelector('.form form input').style.background="white";
+        Array.from(document.getElementsByTagName('li')).forEach((item)=>{
+            item.style.color="black";
+        })
+        e.target.setAttribute('mode', 'light');
+    }
+
+}
 
 
 // initialization
 input.focus();
 function init() {
     rendertodo(todos);
+    mode.addEventListener("click", handlemode);
     form.addEventListener("submit", addtodo);
     list.addEventListener("click", handleclick);
-    last.addEventListener("click", handleclick)
+    last.addEventListener("click", handleclick);
+
 
 }
 init();
